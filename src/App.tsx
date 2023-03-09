279f2199f07ac1aabcde1e8e54a414c5a4214054
anna.zhyla <anna.zh90@gmail.com>
2023-03-09 14:22:36 +0200

    squash commits

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>First commit in main</div>
-        <div>Second commit in main</div>
-        <div>Third commit in main</div>
+        <div>First commit in main + first commit in second branch</div>
+        <div>Second commit in main + second commit in second branch</div>
+        <div>Third commit in main + third commit in second branch</div>
+        <div>Fourth commit in second branch</div>
+
       </header>
     </div>
   );
